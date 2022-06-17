2c2222746ab8718f07d1017918a4b952f2a8b312
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-06-17 13:54:48 +0530

    routes and corresponding controller for getting all posts , getting a single post , making a post added in the backend

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,40 +1,62 @@
+const { validationResult } = require("express-validator");
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post's content",
-        imageUrl: "images/duck.jpg",
-        creator: {
-          name: "John",
-        },
-        createdAt: new Date(),
-      },
-      {
-        _id: "2",
-        title: "Second Post",
-        content: "This is the second post's content",
-        imageUrl: "images/duck.jpg",
-        creator: {
-          name: "John",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Invalid Input Data",
+      errors: errors.array(),
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
-
-  res.status(201).json({
-    message: "Post Created!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-    },
+  const post = new Post({
+    title: title,
+    imageUrl: "images/duck.jpg",
+    content: content,
+    creator: { name: "John" },
   });
+
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post Created!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (post) {
+        res.status(200).json({ post: post, message: "Post found!" });
+      } else {
+        res.status(404).json({ message: "Post not found!" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- Backend/router/feed.js
+++ Backend/router/feed.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const feedController = require("../controllers/feed");
+const { body } = require("express-validator");
 
 const router = express.Router();
-router.get("/post", feedController.getPosts); // getPosts is a function
+router.get("/posts", feedController.getPosts); // getPosts is a function
 
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
+router.get("/post/:postId", feedController.getPost);
 module.exports = router;
