20e01b976b18c073b33044190d0183f57197e61b
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-06-17 13:56:09 +0530

    server-side validation and connecting to database code added

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const feedRoutes = require("./router/feed");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+//cors error removal
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -16,6 +22,19 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080, () => {
-  console.log("server is running on port 8080");
-});
+mongoose
+  .connect(
+    "mongodb+srv://chetan_325:chetan_325@cluster0.aopmq.mongodb.net/messages?retryWrites=true&w=majority"
+  )
+  .then(
+    console.log("Connected to database!"),
+    app.listen(8080, () => {
+      console.log("Connected to the server");
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+  });
+// app.listen(8080, () => {
+//   console.log("server is running on port 8080");
+// });
